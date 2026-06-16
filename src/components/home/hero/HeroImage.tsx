225c437410d053dde6017f6912800083702e0726
vlk2022 <volodymyrkostiuk2021@gmail.com>
2026-06-16 18:26:39 +0300

    images

--- src/components/home/hero/HeroImage.tsx
+++ src/components/home/hero/HeroImage.tsx
@@ -11,6 +11,8 @@ export const HeroImage = () => {
                 sizes="(max-width: 980px) 100vw, 52vw"
                 className="object-cover object-left-center"
             />
+
+            <div className="pointer-events-none absolute left-0 top-0 z-10 h-8 w-full bg-gradient-to-b from-[var(--sirko-parchment-100)] via-[var(--sirko-parchment-100)]/90 to-transparent" />
         </div>
     );
 };
