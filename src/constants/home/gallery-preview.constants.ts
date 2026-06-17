48d6ba4e11067c77ca93508127e943811f6fd171
vlk2022 <volodymyrkostiuk2021@gmail.com>
2026-06-17 14:14:23 +0300

    cards

--- src/constants/home/gallery-preview.constants.ts
+++ src/constants/home/gallery-preview.constants.ts
@@ -7,37 +7,37 @@ export type GalleryPreviewImage = {
 export const GALLERY_PREVIEW_IMAGES: GalleryPreviewImage[] = [
     {
         id: "portrait",
-        src: "/images/sirko/gallery/gallery-01.webp",
+        src: "/images/sirko/gallery/gallery-01.png",
         titleKey: "portrait",
     },
     {
         id: "monument",
-        src: "/images/sirko/gallery/gallery-02.webp",
+        src: "/images/sirko/gallery/gallery-02.png",
         titleKey: "monument",
     },
     {
         id: "grave",
-        src: "/images/sirko/gallery/gallery-03.webp",
+        src: "/images/sirko/gallery/gallery-03.png",
         titleKey: "grave",
     },
     {
         id: "cossacks",
-        src: "/images/sirko/gallery/gallery-04.webp",
+        src: "/images/sirko/gallery/gallery-04.png",
         titleKey: "cossacks",
     },
     {
         id: "illustration",
-        src: "/images/sirko/gallery/gallery-05.webp",
+        src: "/images/sirko/gallery/gallery-05.png",
         titleKey: "illustration",
     },
     {
         id: "sich",
-        src: "/images/sirko/gallery/gallery-06.webp",
+        src: "/images/sirko/gallery/gallery-06.png",
         titleKey: "sich",
     },
     {
         id: "legacy",
-        src: "/images/sirko/gallery/gallery-07.webp",
+        src: "/images/sirko/gallery/gallery-7.png",
         titleKey: "legacy",
     },
 ];
